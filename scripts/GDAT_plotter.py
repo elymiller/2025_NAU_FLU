ec9d883ca8ac582c679f122accd582dd63823a09
Ely F. Miller <efinn2019@gmail.com>
2026-01-16 12:16:40 -0700

    v1 of US model

--- scripts/GDAT_plotter.py
+++ scripts/GDAT_plotter.py
@@ -2,9 +2,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # --- file paths ---
-exp_file = "/Users/l-biosci-posnerlab/Desktop/2025_NAU_FLU/current_job/exp_files/Alabama_flu.exp"
-gdat_file = "/Users/elymiller/Desktop/covid_model_test/Alabama/Results/Alabama_gen70ind11/2025_09_23__16_51_08/Alabama_gen70ind11_Alabama_flu.gdat"  
-#exp_check = "/Users/elymiller/Desktop/covid_model_test/confirm_alabama.exp"
+#exp_file = "/Users/l-biosci-posnerlab/Desktop/2025_NAU_FLU/current_job/exp_files/Alabama_flu.exp"
+exp_file = "/Users/elymiller/Desktop/Current_Research/2025_NAU_FLU/exp_files/US_cases.exp"
+#gdat_file = "/Users/elymiller/Desktop/covid_model_test/Alabama/Results/Alabama_gen70ind11/2025_09_23__16_51_08/Alabama_gen70ind11_Alabama_flu.gdat"  
+gdat_file = "/Users/elymiller/Desktop/Current_Research/2025_NAU_FLU/USA_results/USA/Results/USA_gen28ind3/2026_01_16__12_11_30/USA_gen28ind3_US_cases.gdat"  
+
+##exp_check = "/Users/elymiller/Desktop/covid_model_test/confirm_alabama.exp"
 
 # --- load EXP data ---
 with open(exp_file, "r") as f:
@@ -26,7 +29,7 @@ h_exp = exp["H_weekly"]
 #weeks_check = check["time"]
 #h_check = check["H_weekly"] 
 weeks_mod = gdat["time"]
-h_mod = gdat["H_obs"] 
+h_mod = gdat["H_weekly"] 
 
 
 # --- plot ---
@@ -40,4 +43,5 @@ plt.ylabel("Weekly hospitalizations")
 plt.title("New Model Check (Alabama)")
 plt.legend()
 plt.tight_layout()
-plt.savefig("/Users/l-biosci-posnerlab/Desktop/2025_NAU_FLU/current_job/DE_checks/alabama_DE.png", dpi=500)
+#plt.savefig("/Users/l-biosci-posnerlab/Desktop/2025_NAU_FLU/current_job/DE_checks/alabama_DE.png", dpi=500)
+plt.savefig("/Users/elymiller/Desktop/Current_Research/2025_NAU_FLU/USA_results/USA_DE.png", dpi=500)
